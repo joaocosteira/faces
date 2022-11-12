b05a4c21da81711e5a7ea032fe187af62aff719c
Costeira <joaocurveira13@gmail.com>
2022-11-12 20:58:38 +0000

    faces

--- script.js
+++ script.js
@@ -1,27 +1,82 @@
-const menu = document.querySelector('.menu');
+const menu = document.querySelector('.ball');
+const audio = document.querySelector('.audio');
 
-tl = gsap.timeline({ paused : true });
-tl.to(".menu",{ duration: 1, width: "400vw", height: "400vh"})
-tl.to(".menu",{ duration: 0, top: 0, left: 0,translateX : "50%", translateY : "50%",width: "100vw", height: "100vh", borderRadius : 0, ease: Power2.linear})
-tl.to(
-    ".menu-content",
-    {
+let tl = gsap.timeline({paused : true});
+tl.fromTo(".ball",{
+    position: "fixed",
+    top: "90vh",
+    left: "80vw",
+    zIndex: 3
+},{ duration : 1, width: "400vw", height: "400vw"});
+tl.to(".ball",{
+    duration: 0,
+    width: "100vw", 
+    height: "100vh",
+    borderRadius : 0,
+    x:"50%",
+    y:"50%",
+    top: 0,
+    left: 0,
+    position: "absolute",
+    zIndex: 3
+})
+tl.fromTo(".menu-content",{
+
+    display: "none",
+    duration: 0,
+    gap: 0,
+    padding: 0
+    },{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: "1rem",
+        duration: "100ms",
+        padding: "2rem"
+    });
+
+tl.fromTo(".menu-item",{
+        opacity: 0
+    },{
         opacity: 1,
-        marginBottom: 0,
-        duration: 2,
-        ease: Power2.easeInOut,
-    }
-);
+        duration: 1,
+        stagger: 0.2,
+    })
 
-const handleClick = () => {
+tl.fromTo(".x",{
+        opacity: 0
+    },{
+        alignSelf: "flex-end",
+        opacity: 1,
+        duration: 1,
+        stagger: 0.1,
+    })    
+
+const toggleMenu = () => {
+
     menu.dataset.status = menu.dataset.status === "closed" ? "opened" : "closed";
+    
     if(menu.dataset.status === "opened"){
-        tl.timeScale(1).play();
+        tl.play().timeScale(1);
     }else{
-        tl.timeScale(3.5).reverse();
+        tl.timeScale(2);
+        tl.reverse();
     }
 }
-//menu.addEventListener("click", () => {
-//     console.log("Clicked Me!!!")
-//     tl.play();
-// }) 
+
+
+const handleAudio = () => {
+    audio.dataset.music = audio.dataset.music === "paused" ? "playing" : "paused";
+}
+
+
+//ver como mudar o source da img
+const faces = document.querySelectorAll('.face')
+let currentFace = 0;
+
+setInterval(() => {
+    faces[currentFace].dataset.imgstatus="inactive";
+    currentFace = (currentFace + 1) % faces.length;
+    faces[currentFace].dataset.imgstatus="active";
+}, 100)
